f5b14b147f6395d8ed6c81917428965220b5c192
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-05-14 23:33:00 +0530

    feat: Added Navbar Responsive

---
+++ apps/User/app/about/page.tsx
@@ -0,0 +1,9 @@
+
+
+const About = () => {
+    return <div className="flex"> 
+        <h1>About</h1>
+    </div>
+} 
+
+export default About 
--- packages/ui/src/Navbar.tsx
+++ packages/ui/src/Navbar.tsx
@@ -1,49 +1,89 @@
-"use client"
-
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
-import { Button } from "./shad/ui/button"; 
-import { ModeToggle } from "./Toggle/ToggleTheme";
-import { AlignJustify, X } from "lucide-react"; 
+import { AlignJustify, X } from "lucide-react";
+import { Button } from "./shad/ui/button";
+import Sidebar from "./Sidebar";
+import LinksDropdown from "./dropdown/LinksDropdown";
 
-const Navbar = () => { 
-  const [Toggle,setToggle] = useState(false)
+import { NavbarLinks } from "../../data/Navbar-links";
+
+const subLinks = [
+  {
+    title: "Python",
+    link: "/catalog/python",
+  },
+  {
+    title: "Web Dev",
+    link: "/catalog/web-development",
+  },
+];
+
+const Navbar = ({ children }: { children: React.ReactNode }) => {
+  const [toggle, setToggle] = useState(false);
+  const router = usePathname();
+
+  const token = null;
+
   return (
     <div>
-      <div className="flex justify-around font-poppins mt-4 items-center shadow-lg shadow-indigo-500/50 pb-3 ">
-        <div className="text-[20px] sm:text-[25px] md:text-[30px] font-semibold cursor-pointer">
-          <h1>LearnNest</h1>
-        </div>
-        <div>
-          <ul className="hidden sm:flex gap-5 sm:text-[15px] md:text-[20px] text-[#7C7575] cursor-pointer ">
-            <li className="text-black dark:text-white">Home</li>
-            <li className="hover:text-black dark:hover:text-white">Catalog</li>
-            <li className="hover:text-black dark:hover:text-white">About us</li>
-            <li className="hover:text-black dark:hover:text-white">Contact us</li>
-          </ul> 
-        </div>
-        <div className="flex gap-2 items-center">
-          <Button className="bg-white text-black shadow-none border-none rounded-[15px] sm:p-5 sm:pt-3 sm:h-[43px] hover:bg-slate-100 text-[15px] sm:text-[20px] bg-transparent dark:text-white dark:hover:bg-slate-800 focus:ring-2 ">
-            Login
-          </Button>
-          <Button className="bg-[#9C49CF] text-[15px] sm:text-[20px] shadow-none border-none rounded-[15px] sm:p-4 sm:pt-3  sm:h-[43px] hover:bg-[#671997] dark:text-white focus:ring-2 ">
-            Signup
-          </Button>
-          <div className="block sm:hidden"> 
-            <Button className="w-12 bg-transparent focus:bg-transparent hover:bg-transparent focus:ring-2" onClick={() => {setToggle(!Toggle)}}> 
-              {Toggle? <X  className="text-black dark:text-white"/> : <AlignJustify className="text-black dark:text-white"/> } 
+      <div className="flex h-20 items-center justify-center shadow-lg shadow-indigo-500/50">
+        <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+          <Link href="/">
+            <h1 className="text-3xl font-semibold">LearnNest</h1>
+          </Link>
+          <nav>
+            <ul className="hidden md:flex gap-x-6 text-lg text-[#7C7575]">
+              {NavbarLinks.map((link, index) => (
+                <li key={index}>
+                  {link.title === "Catalog" ? (
+                    <LinksDropdown Title={link.title} links={subLinks} />
+                  ) : (
+                    <Link href={link.path || "/"}>
+                      <p
+                        className={
+                          router === link.path
+                            ? "text-[#9C49CF]"
+                            : "text-white hover:text-[#9C49CF]"
+                        }
+                      >
+                        {link.title}
+                      </p>
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="hidden md:flex gap-3">
+            {token === null && (
+              <Button className="bg-transparent h-12 text-white rounded-lg hover:bg-transparent focus:ring-2">
+                Login
+              </Button>
+            )}
+            {token === null && (
+              <Button className="bg-[#9C49CF] h-12 text-white rounded-lg hover:bg-[#671997] focus:ring-2">
+                Signup
+              </Button>
+            )}
+          </div>
+          <div className="block sm:hidden">
+            <Button
+              onClick={() => setToggle(!toggle)}
+              className="h-12 bg-black text-white rounded-lg focus:bg-black"
+            >
+              {toggle ? <X color="white" /> : <AlignJustify color="white" />}
             </Button>
           </div>
-          <ModeToggle />
         </div>
       </div>
-      {Toggle? <div className="bg-white dark:bg-transparent mb-5 py-3 shadow  ">
-          <ul className="flex flex-col gap-5 text-[20px] text-[#7C7575] cursor-pointer items-center">
-            <li className="text-black dark:text-white">Home</li>
-            <li className="hover:text-black dark:hover:text-white">Catalog</li>
-            <li className="hover:text-black dark:hover:text-white">About us</li>
-            <li className="hover:text-black dark:hover:text-white">Contact us</li>
-          </ul> 
-        </div>: null}
+      <div className="relative">
+        {toggle && <Sidebar onClick={() => {
+          setToggle(!toggle)
+        }} />}
+        {children}
+      </div>
     </div>
   );
 };
