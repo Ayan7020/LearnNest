26882aeb394d3410c193d2a8ac05ea59be2a0550
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-05-15 14:08:37 +0530

    Feat: DarkmodeToggle/Resoponsiveness-nav

--- packages/ui/src/Sidebar.tsx
+++ packages/ui/src/Sidebar.tsx
@@ -4,6 +4,7 @@ import Dropdown from "./dropdown/Dropdown";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./shad/ui/button";
+import React, { useState, useEffect } from 'react';
 
 const subLinks = [
   {
@@ -16,21 +17,37 @@ const subLinks = [
   },
 ];
 
+
+
 const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: "-10%" },
+    open: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.3, // Adjust the duration of the animation
+        },
+    },
+    closed: {
+        opacity: 0,
+        x: "-100%",
+        transition: {
+            duration: 0.3, // Adjust the duration of the animation
+        },
+    },
 };
 
-const Sidebar = ({onClick}:{
-  onClick: () => void
+const Sidebar = ({onClick,State}:{
+  onClick: () => void,
+  State: boolean
 }) => {
   const router = usePathname();
-  const isOpen = true; 
+  const isOpen = State; 
+  const [shouldRender, setShouldRender] = useState(true);
 
   return (
     <motion.div
-      className="fixed bg-black w-[50%] h-[100%] text-white flex flex-col gap-4 pt-20 top-0"
-      initial="closed"
+      className=" fixed bg-white dark:bg-black w-[50%] h-[100%] dark:text-white flex flex-col gap-4 pt-20 top-0"
+      initial={isOpen? "closed":"open"}
       animate={isOpen ? "open" : "closed"}
       variants={variants}
     >
@@ -40,10 +57,9 @@ const Sidebar = ({onClick}:{
             {link.title === "Catalog" ? (
               <Dropdown Title={link.title} links={subLinks} />
             ) : (
-              <Link href={link?.path || "/"} onClick={onClick}>
-                {/* Use <p> tag for clickable text */}
+              <Link href={link?.path || "/"} onClick={onClick}> 
                 <p
-                  className={`cursor-pointer ${router === link?.path ? "text-[#9C49CF]" : "text-white hover:text-[#9C49CF]"} flex flex-col items-center`}
+                  className={`cursor-pointer ${router === link?.path ? "text-[#9C49CF]" : "dark:text-white hover:text-[#9C49CF]"} flex flex-col items-center`}
                 >
                   {link.title}
                 </p>
@@ -52,11 +68,11 @@ const Sidebar = ({onClick}:{
           </li>
         ))}
       </ul>
-      <div className="flex flex-col gap-2 pt-4 border-t-2 border-white items-center">
-        <Button className="bg-transparent border border-white text-white rounded-[15px] px-6 py-3 hover:bg-transparent hover:text-white  transition-colors duration-300 ease-in-out focus:ring-2">
+      <div className="flex flex-col gap-2 pt-4 border-t-2 border-black dark:border-white items-center">
+        <Button className="bg-transparent border border-white text-black dark:text-white rounded-[15px] px-6 py-3 hover:bg-transparent ring-1 dark:hover:text-white  transition-colors duration-300 ease-in-out focus:ring-2 w-[70%] ">
           Login
         </Button>
-        <Button className="bg-[#9C49CF] text-white rounded-[15px] px-6 py-3 hover:bg-[#671997] transition-colors duration-300 ease-in-out">
+        <Button className="bg-[#9C49CF] text-white rounded-[15px] px-6 py-3 hover:bg-[#671997] transition-colors duration-300 ease-in-out w-[70%]">
           Signup
         </Button>
       </div>
--- packages/ui/src/dropdown/LinksDropdown.tsx
+++ packages/ui/src/dropdown/LinksDropdown.tsx
@@ -8,11 +8,11 @@ interface LinksDropdownProps {
 }
 const LinksDropdown = ({ Title, links }: LinksDropdownProps) => {
   return (
-    <div className="relative flex items-center gap-2 group cursor-pointer text-white">
+    <div className="relative flex items-center gap-2 group cursor-pointer text-black dark:text-white ">
       <p>{Title}</p>
       <ChevronDown />
       <div
-        className={`invisible absolute left-[50%] translate-x-[-49%] ${links.length ? "translate-y-[15%]" : "translate-y-[40%]"}  top-[50%] z-50 flex flex-col items-center rounded-md bg-slate-900 p-4 text-white opacity-0 transition-all duration-200 group-hover:visible  group-hover:opacity-100 lg:w-[300px] `}
+        className={`invisible absolute left-[50%] translate-x-[-49%] ${links.length ? "translate-y-[15%]" : "translate-y-[40%]"}  top-[50%] z-50 flex flex-col items-center rounded-md bg-slate-900 p-4 text-white opacity-0 transition-all duration-200 group-hover:visible  group-hover:opacity-100 w-[200px] lg:w-[300px] `}
       >
         <div className="absolute left-[50%] top-0 translate-x-[80%] translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-slate-900"></div>
         {links.length ? (
