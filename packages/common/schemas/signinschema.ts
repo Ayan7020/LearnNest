20e417c2e3d8ca8f67cdd528cf00a6b7debf43ad
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-05-22 17:13:52 +0530

    Bug: Password-Matching/ERROR

--- packages/common/schemas/signinschema.ts
+++ packages/common/schemas/signinschema.ts
@@ -5,7 +5,10 @@ export const SignInSchema = z.object({
     LastName: z.string().min(1,{message: "Name is Required"}),
     email: z.string().email("Invalid email address"), 
     password: z.string().min(8,{message: "Password must be 8 character"}),
-    confirmPassword: z.string().min(1,{message: "Confirm Password is Required"}),
+    confirmPassword: z.string().min(1,{message: "Confirm Password is Required"}) 
+}).refine(data => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
 })
 
 export type SignInSchemaValue = z.infer<typeof SignInSchema>; 
