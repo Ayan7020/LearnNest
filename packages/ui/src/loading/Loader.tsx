863eec05d9737f2c3b6a76299f2840c0b1b1e6cb
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-05-31 19:00:02 +0530

    Fix: Auth/Role-Based Auth

--- packages/ui/src/loading/Loader.tsx
+++ packages/ui/src/loading/Loader.tsx
@@ -1,8 +1,9 @@
+"use client";
 import { LoaderCircle } from "lucide-react";
 
 
-const Loading = () => {
+const Loader = () => {
     return <div className="animate-spin w-[100px] flex flex-col items-center shadow-[10px_-5px_50px_-5px]  shadow-blue-200 rounded-full "><LoaderCircle width={50} height={100}/></div> 
 }
 
-export default Loading;
+export default Loader;
