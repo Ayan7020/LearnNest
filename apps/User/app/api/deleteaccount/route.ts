69e48bb2b23de477c38e33d861db9de9b662a0d5
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2024-06-07 16:19:18 +0530

    Feat: Account deleted

--- apps/User/app/api/deleteaccount/route.ts
+++ apps/User/app/api/deleteaccount/route.ts
@@ -1,10 +1,9 @@
-import { NextApiRequest } from "next";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { authOptions } from "../../../lib/auth";
 import db from "@repo/db/clients";
 
-const deleteAccount = async (req: NextApiRequest): Promise<NextResponse> => {
+const deleteAccount = async (req: any): Promise<NextResponse> => {
     try {
         const session = await getServerSession(authOptions);
         const userEmail = session?.user?.email;
